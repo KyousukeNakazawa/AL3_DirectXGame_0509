1aef5a2e8c777e16cc09019ed59508dcc00c2b8d
kyousukenakazawa <k021g1400@g.neec.ac.jp>
2022-05-31 14:02:25 +0900

    関数完成

---
+++ MathUtility.cpp
@@ -0,0 +1,65 @@
+﻿#include "TextureManager.h"
+#include "AxisIndicator.h"
+#include "PrimitiveDrawer.h"
+#include "MathUtility.h"
+#include <cassert>
+
+//Matrix4 MathUtility::Matrix4Identity() {
+//	Matrix4 matrix;
+//	matrix.m[0][0] = 1;
+//	matrix.m[1][1] = 1;
+//	matrix.m[2][2] = 1;
+//	matrix.m[3][3] = 1;
+//
+//	return matrix;
+//}
+
+Matrix4 MathUtility::Matrix4Scaling(Vector3 scale) {
+	Matrix4 matScale = MathUtility::Matrix4Identity();
+	matScale.m[0][0] = scale.x;
+	matScale.m[1][1] = scale.y;
+	matScale.m[2][2] = scale.z;
+
+	return matScale;
+}
+
+Matrix4 MathUtility::Matrix4Rotation(Vector3 rotation) {
+	//合成用回転行列を宣言
+	Matrix4 matRot = MathUtility::Matrix4Identity();
+
+	//各軸用回転行列を宣言
+	Matrix4 matRotX, matRotY, matRotZ;
+
+	matRotX = MathUtility::Matrix4Identity();
+	matRotY = MathUtility::Matrix4Identity();
+	matRotZ = MathUtility::Matrix4Identity();
+
+	matRotZ.m[0][0] = cos(rotation.z);
+	matRotZ.m[0][1] = sin(rotation.z);
+	matRotZ.m[1][0] = -sin(rotation.z);
+	matRotZ.m[1][1] = cos(rotation.z);
+
+	matRotX.m[1][1] = cos(rotation.x);
+	matRotX.m[1][2] = sin(rotation.x);
+	matRotX.m[2][1] = -sin(rotation.x);
+	matRotX.m[2][2] = cos(rotation.x);
+
+	matRotY.m[0][0] = cos(rotation.y);
+	matRotY.m[0][2] = -sin(rotation.y);
+	matRotY.m[2][0] = sin(rotation.y);
+	matRotY.m[2][2] = cos(rotation.y);
+
+	matRot = matRotZ * matRotX * matRotY;
+
+	return matRot;
+}
+
+Matrix4 MathUtility::Matrix4Translation(Vector3 translation) {
+	Matrix4 matTrans = MathUtility::Matrix4Identity();
+
+	matTrans.m[3][0] = translation.x;
+	matTrans.m[3][1] = translation.y;
+	matTrans.m[3][2] = translation.z;
+
+	return matTrans;
+}
--- math/MathUtility.h
+++ math/MathUtility.h
@@ -5,61 +5,62 @@
 
 namespace MathUtility {
 
-const float PI = 3.141592654f;
+	const float PI = 3.141592654f;
 
-// 零ベクトルを返す
-const Vector3 Vector3Zero();
-// 2ベクトルが一致しているか調べる
-bool Vector3Equal(const Vector3& v1, const Vector3& v2);
-// ノルム(長さ)を求める
-float Vector3Length(const Vector3& v);
-// 正規化する
-Vector3& Vector3Normalize(Vector3& v);
-// 内積を求める
-float Vector3Dot(const Vector3& v1, const Vector3& v2);
-// 外積を求める
-Vector3 Vector3Cross(const Vector3& v1, const Vector3& v2);
+	// 零ベクトルを返す
+	const Vector3 Vector3Zero();
+	// 2ベクトルが一致しているか調べる
+	bool Vector3Equal(const Vector3& v1, const Vector3& v2);
+	// ノルム(長さ)を求める
+	float Vector3Length(const Vector3& v);
+	// 正規化する
+	Vector3& Vector3Normalize(Vector3& v);
+	// 内積を求める
+	float Vector3Dot(const Vector3& v1, const Vector3& v2);
+	// 外積を求める
+	Vector3 Vector3Cross(const Vector3& v1, const Vector3& v2);
 
-// 2項演算子オーバーロード
-const Vector3 operator+(const Vector3& v1, const Vector3& v2);
-const Vector3 operator-(const Vector3& v1, const Vector3& v2);
-const Vector3 operator*(const Vector3& v, float s);
-const Vector3 operator*(float s, const Vector3& v);
-const Vector3 operator/(const Vector3& v, float s);
+	// 2項演算子オーバーロード
+	const Vector3 operator+(const Vector3& v1, const Vector3& v2);
+	const Vector3 operator-(const Vector3& v1, const Vector3& v2);
+	const Vector3 operator*(const Vector3& v, float s);
+	const Vector3 operator*(float s, const Vector3& v);
+	const Vector3 operator/(const Vector3& v, float s);
 
-// 単位行列を求める
-Matrix4 Matrix4Identity();
-// 転置行列を求める
-Matrix4 Matrix4Transpose(const Matrix4& m);
+	// 単位行列を求める
+	Matrix4 Matrix4Identity();
+	// 転置行列を求める
+	Matrix4 Matrix4Transpose(const Matrix4& m);
 
-// 拡大縮小行列の作成
-Matrix4 Matrix4Scaling(float sx, float sy, float sz);
+	// 拡大縮小行列の作成
+	Matrix4 Matrix4Scaling(Vector3 scale);
 
-// 回転行列の作成
-Matrix4 Matrix4RotationX(float angle);
-Matrix4 Matrix4RotationY(float angle);
-Matrix4 Matrix4RotationZ(float angle);
+	// 回転行列の作成
+	Matrix4 Matrix4Rotation(Vector3 rotation);
+	Matrix4 Matrix4RotationX(float angle);
+	Matrix4 Matrix4RotationY(float angle);
+	Matrix4 Matrix4RotationZ(float angle);
 
-// 平行移動行列の作成
-Matrix4 Matrix4Translation(float tx, float ty, float tz);
+	// 平行移動行列の作成
+	Matrix4 Matrix4Translation(Vector3 translation);
 
-// ビュー行列の作成
-Matrix4 Matrix4LookAtLH(const Vector3& eye, const Vector3& target, const Vector3& up);
-// 並行投影行列の作成
-Matrix4 Matrix4Orthographic(
-  float viewLeft, float viewRight, float viewBottom, float viewTop, float nearZ, float farZ);
-// 透視投影行列の作成
-Matrix4 Matrix4Perspective(float fovAngleY, float aspectRatio, float nearZ, float farZ);
+	// ビュー行列の作成
+	Matrix4 Matrix4LookAtLH(const Vector3& eye, const Vector3& target, const Vector3& up);
+	// 並行投影行列の作成
+	Matrix4 Matrix4Orthographic(
+		float viewLeft, float viewRight, float viewBottom, float viewTop, float nearZ, float farZ);
+	// 透視投影行列の作成
+	Matrix4 Matrix4Perspective(float fovAngleY, float aspectRatio, float nearZ, float farZ);
 
-// 座標変換（w除算なし）
-Vector3 Vector3Transform(const Vector3& v, const Matrix4& m);
-// 座標変換（w除算あり）
-Vector3 Vector3TransformCoord(const Vector3& v, const Matrix4& m);
-// ベクトル変換
-Vector3 Vector3TransformNormal(const Vector3& v, const Matrix4& m);
+	// 座標変換（w除算なし）
+	Vector3 Vector3Transform(const Vector3& v, const Matrix4& m);
+	// 座標変換（w除算あり）
+	Vector3 Vector3TransformCoord(const Vector3& v, const Matrix4& m);
+	// ベクトル変換
+	Vector3 Vector3TransformNormal(const Vector3& v, const Matrix4& m);
 
-// 2項演算子オーバーロード
-Matrix4 operator*(const Matrix4& m1, const Matrix4& m2);
-Vector3 operator*(const Vector3& v, const Matrix4& m);
+	// 2項演算子オーバーロード
+	Matrix4 operator*(const Matrix4& m1, const Matrix4& m2);
+	Vector3 operator*(const Vector3& v, const Matrix4& m);
 
 } // namespace MathUtility
